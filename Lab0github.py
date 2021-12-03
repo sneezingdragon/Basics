b5d91526bf49b6657ff881ef0113b07876f927d4
Sarah Brewer <94143996+sneezingdragon@users.noreply.github.com>
2021-12-02 17:21:25 -0700

    Add files via upload

---
+++ Lab0github.py
@@ -0,0 +1,12 @@
+import math
+List = ["roads", "cities", "counties", "states"]
+list2 = []
+for i in List:
+    list2.append(i + '.txt')
+print(list2)
+x = math.pi
+y = 0
+while x**y < 10000:
+    x**y
+    print(x**y)
+    y = y + 1
